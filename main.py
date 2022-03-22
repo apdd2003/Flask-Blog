b349d4765c3e84604b4e683750b24c0f01dc49e5
samsk <apdd2003@yahoo.co.in>
2022-03-22 12:49:21 +0530

    update requirements file

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import os
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-print(os.environ.get("SECRET_KEY"))
+# print(os.environ.get("SECRET_KEY"))
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
