9a1a471fe06b4e68e1540572f0588223d4cb1ccd
samsk <apdd2003@yahoo.co.in>
2022-04-06 22:58:42 +0530

    edit password db storage length increased

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    password = db.Column(db.String(255))
     name = db.Column(db.String(100))
 
     # This will act like a List of BlogPost objects attached to each User.
