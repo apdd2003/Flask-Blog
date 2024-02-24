169f95c51c4615271e55c63ea084f527c6b9c34b
sayeemuddin <apdd2003@yahoo.co.in>
2024-02-24 23:43:29 +0530

    remove scheduler refs

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ app.register_blueprint(error_pages)
 
 ckeditor = CKEditor(app)
 Bootstrap(app)
-scheduler = APScheduler()
+# scheduler = APScheduler()
 # gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
 #                     base_url=None)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-scheduler.init_app(app)
+# scheduler.init_app(app)
 app.testing = True
 client = app.test_client()
 
